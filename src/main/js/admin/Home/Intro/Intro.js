5fed74fcc7ee3f189c87a0a6ce87882680dda542
Antonio Alonzi <antonioalonzi85@gmail.com>
2020-05-10 20:31:02 +0100

    Updated link to matag.

--- src/main/js/admin/Home/Intro/Intro.js
+++ src/main/js/admin/Home/Intro/Intro.js
@@ -8,7 +8,7 @@ export default class Intro extends Component {
           Welcome to Matag, a web based version of <strong>Magic: The Gathering</strong> where you can play using just your browser.
         </p>
         <p>
-          The game is open source and you can find the code and contribute or contact the creators at <a href='https://github.com/antonioalonzi/matag' target='_blank'>matag</a> github.<br/>
+          The game is open source and you can find the code and contribute or contact the creators at <a href='https://github.com/MatagTheGame/matag-the-game' target='_blank'>Matag: The Game</a> github.<br/>
           (Please note that we are not affiliated in any way with the MTG creators, nor we claim any copyright over their game or art assets.)
         </p>
         <p>
