848ca628a22e23df52031fcc96a0b1527b0ea0c2
Antonio Alonzi <antonioalonzi85@gmail.com>
2026-01-04 18:41:47 +0000

    Improve tests and attempt to fix logout again

--- src/main/js/admin/Auth/Logout/Logout.js
+++ src/main/js/admin/Auth/Logout/Logout.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {Link, useNavigate} from 'react-router-dom'
 import AuthHelper from 'admin/Auth/AuthHelper'
 import ApiClient from 'admin/utils/ApiClient'
-import {APP_BASE_PATH} from 'admin/AdminApp'
 
 export default function Logout() {
   const navigate = useNavigate()
