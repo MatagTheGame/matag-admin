8f80402de3ff7136a733f3e16d95dd8418168fc6
Antonio Alonzi <antonioalonzi85@gmail.com>
2026-01-04 18:18:26 +0000

    Fix Logout

--- src/main/js/admin/Auth/Logout/Logout.js
+++ src/main/js/admin/Auth/Logout/Logout.js
@@ -1,20 +1,21 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 import AuthHelper from 'admin/Auth/AuthHelper'
 import ApiClient from 'admin/utils/ApiClient'
+import {APP_BASE_PATH} from 'admin/AdminApp'
 
-export default class Logout extends Component {
-  logout() {
+export default function Logout() {
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
     ApiClient.getNoJson('/auth/logout')
       .then(() => {
         AuthHelper.removeToken()
-        window.location.href = '/'
+        navigate(APP_BASE_PATH + '/ui')
       })
   }
 
-  render() {
-    return (
-      <Link to='#' onClick={this.logout}>Logout</Link>
-    )
-  }
+  return (
+    <Link to="#" onClick={handleLogout}>Logout</Link>
+  )
 }
