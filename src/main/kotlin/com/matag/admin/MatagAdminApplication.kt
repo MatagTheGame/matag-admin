1e88b331c897b318356efc958174fdffd86c03f6
Antonio Alonzi <antonioalonzi85@gmail.com>
2026-01-19 21:41:29 +0000

    fix app startup

--- src/main/kotlin/com/matag/admin/MatagAdminApplication.kt
+++ src/main/kotlin/com/matag/admin/MatagAdminApplication.kt
@@ -1,11 +1,11 @@
 package com.matag.admin
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class MatagAdminApplication {
-    fun main(args: Array<String>) {
-        SpringApplication.run(MatagAdminApplication::class.java, *args)
-    }
+open class MatagAdminApplication
+
+fun main(args: Array<String>) {
+    runApplication<MatagAdminApplication>(*args)
 }
