449d6a263b5d039cb189fff4cff7dd5d823d78ca
Antonio Alonzi <antonioalonzi85@gmail.com>
2026-01-21 21:14:28 +0000

    Modern react Login

--- src/main/js/admin/Auth/Login/Login.js
+++ src/main/js/admin/Auth/Login/Login.js
@@ -1,119 +1,78 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import get from 'lodash/get'
-import {bindActionCreators} from 'redux'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import ApiClient from 'admin/utils/ApiClient'
-import history from 'admin/utils/history'
 import AuthHelper from '../AuthHelper'
-import './login.scss'
 import FormError from 'admin/Form/FormError'
 import Loader from 'admin/Common/Loader'
 
+const Login = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-class Login extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      emailOrUsername: '',
-      password: ''
-    }
-    this.handleChangeEmailOrUsername = this.handleChangeEmailOrUsername.bind(this)
-    this.handleChangePassword = this.handleChangePassword.bind(this)
-    this.handleLogin = this.handleLogin.bind(this)
-    this.handleLoginAsGuest = this.handleLoginAsGuest.bind(this)
-  }
+  const [form, setForm] = useState({ emailOrUsername: '', password: '' })
 
-  componentDidUpdate(prevProps, prevState, snapshot) { // eslint-disable-line
-    if (this.props.isLoggedIn) {
-      history.push('/ui')
-    }
-  }
-
-  handleChangeEmailOrUsername(event) {
-    this.setState({emailOrUsername: event.target.value})
-  }
-
-  handleChangePassword(event) {
-    this.setState({password: event.target.value})
-  }
-
-  login(emailOrUsername, password) {
-    const request = {
-      emailOrUsername: emailOrUsername,
-      password: password
-    }
-
-    this.props.loginLoading()
-    ApiClient.post('/auth/login', request)
-      .then(response => this.props.loginResponse(response))
-  }
-
-  handleLogin(event) {
-    event.preventDefault()
-    this.login(this.state.emailOrUsername, this.state.password)
-  }
-
-  handleLoginAsGuest() {
-    this.login('guest@matag.com', 'password')
-  }
-
-  render() {
-    return (
-      <section id='login'>
-        <div id='login-container'>
-          <h2>Login</h2>
-          <form className='matag-form' onSubmit={this.handleLogin}>
-            <div className='grid grid-label-value'>
-              <label htmlFor='email-or-username'>Email or Username: </label>
-              <input type='text' id='email-or-username' name='email-or-username' value={this.state.emailOrUsername} onChange={this.handleChangeEmailOrUsername}/>
-            </div>
-            <div className='grid grid-label-value'>
-              <label htmlFor='password'>Password: </label>
-              <input type='password' id='password' name='password' value={this.state.password} onChange={this.handleChangePassword}/>
-            </div>
-            <FormError error={this.props.error} />
-            <div className='grid three-columns'>
-              <div/>
-              <div className='form-buttons'>
-                <input type='submit' value='Login'/>
-                <div className='or'>or</div>
-                <input type='button' value='Login as Guest' onClick={this.handleLoginAsGuest}/>
-              </div>
-              {this.props.loading && <Loader center/>}
-            </div>
-          </form>
-        </div>
-      </section>
-    )
-  }
-}
-
-const loginLoading = () => {
-  return {
-    type: 'LOGIN_LOADING'
-  }
-}
-
-const loginResponse = (response) => {
-  return {
-    type: 'LOGIN_RESPONSE',
-    value: response
-  }
-}
-
-const mapStateToProps = state => {
-  return {
+  const { isLoggedIn, loading, error } = useSelector(state => ({
     isLoggedIn: AuthHelper.isLoggedIn(state),
-    loading: get(state, 'login.loading', false),
-    error: get(state, 'login.error', null),
+    loading: state.login?.loading || false,
+    error: state.login?.error || null
+  }))
+
+  useEffect(() => {
+    if (isLoggedIn) navigate('/ui')
+  }, [isLoggedIn, navigate])
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setForm(prev => ({ ...prev, [name]: value }))
   }
+
+  const login = async (emailOrUsername, password) => {
+    dispatch({ type: 'LOGIN_LOADING' })
+    try {
+      const response = await ApiClient.post('/auth/login', { emailOrUsername, password })
+      dispatch({ type: 'LOGIN_RESPONSE', value: response })
+    } catch (err) {
+      // Error handling here
+    }
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    login(form.emailOrUsername, form.password)
+  }
+
+  const handleLoginAsGuest = () => {
+    login('guest@matag.com', 'password')
+  }
+
+  return (
+    <section id='login'>
+      <div id='login-container'>
+        <h2>Login</h2>
+        <form className='matag-form' onSubmit={handleLogin}>
+          <div className='grid grid-label-value'>
+            <label htmlFor='email-or-username'>Email or Username: </label>
+            <input type='text' id='email-or-username' name='email-or-username' value={form.emailOrUsername} onChange={handleChange}/>
+          </div>
+          <div className='grid grid-label-value'>
+            <label htmlFor='password'>Password: </label>
+            <input type='password' id='password' name='password' value={form.password} onChange={handleChange}/>
+          </div>
+          <FormError error={error} />
+          <div className='grid three-columns'>
+            <div />
+            <div className='form-buttons'>
+              <input type='submit' value='Login' />
+              <div className='or'>or</div>
+              <input type='button' value='Login as Guest' onClick={handleLoginAsGuest} />
+            </div>
+            {loading && <Loader center />}
+          </div>
+        </form>
+      </div>
+    </section>
+  )
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    loginLoading: bindActionCreators(loginLoading, dispatch),
-    loginResponse: bindActionCreators(loginResponse, dispatch)
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+export default Login
