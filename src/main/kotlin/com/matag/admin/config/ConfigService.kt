418cf911aa2a16b6ef2ba288cdea481ae33f06fb
Antonio Alonzi <antonioalonzi85@gmail.com>
2026-01-21 19:27:24 +0000

    Remove Map warnings

--- src/main/kotlin/com/matag/admin/config/ConfigService.kt
+++ src/main/kotlin/com/matag/admin/config/ConfigService.kt
@@ -2,32 +2,31 @@ package com.matag.admin.config
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import java.util.Map
 
 @Component
 class ConfigService(
-    @Value("\${matag.name}")
+    @param:Value("\${matag.name}")
     val matagName: String,
 
-    @Value("\${matag.game.path}")
+    @param:Value("\${matag.game.path}")
     val matagGamePath: String,
 
-    @Value("\${matag.admin.path}")
+    @param:Value("\${matag.admin.path}")
     val matagAdminPath: String,
 
-    @Value("\${matag.admin.externalUrl}")
+    @param:Value("\${matag.admin.externalUrl}")
     val matagExternalUrl: String,
 
-    @Value("\${matag.admin.password}")
+    @param:Value("\${matag.admin.password}")
     val matagAdminPassword: String,
 
-    @Value("\${matag.email.username}")
+    @param:Value("\${matag.email.username}")
     val matagSupportEmail: String
 ) {
-    fun getConfig() = Map.of(
-            "matagName", matagName,
-            "matagAdminUrl", matagAdminPath,
-            "matagGameUrl", matagGamePath,
-            "matagSupportEmail", matagSupportEmail
+    fun getConfig() = mapOf(
+            "matagName" to matagName,
+            "matagAdminUrl" to matagAdminPath,
+            "matagGameUrl" to matagGamePath,
+            "matagSupportEmail" to matagSupportEmail
         )
 }
